bfcbd2dff0ccd65b5afcb7c101c51332954bb34d
Herfando <herfando_91@yahoo.com>
2026-02-21 03:53:34 +0700

    create endpoint swagger auth login register

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -3,10 +3,65 @@ import { register, login } from "../controllers/authController";
 
 const router = Router();
 
-// endpoint register
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Endpoint authentication (register & login)
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register user baru
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: fando
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       201:
+ *         description: User berhasil dibuat
+ *       400:
+ *         description: Request invalid / username sudah ada
+ */
 router.post("/register", register);
 
-// endpoint login
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: fando
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: Login berhasil
+ *       401:
+ *         description: Username atau password salah
+ */
 router.post("/login", login);
 
 export default router;
