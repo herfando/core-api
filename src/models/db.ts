86a6189c1b06508b2318ad2a524afe868808b261
Herfando <herfando_91@yahoo.com>
2026-02-22 19:07:45 +0700

    edit package.json, atuhcontroller, readme

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,14 +1,9 @@
-import pkg from "pg";
-import dotenv from "dotenv";
+import postgres from 'postgres';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { Pool } = pkg;
+const connectionString = process.env.DATABASE_URL!;
+const sql = postgres(connectionString);
 
-export const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+export default sql;
--- src/server.ts
+++ src/server.ts
@@ -8,23 +8,19 @@ dotenv.config();
 
 const app = express();
 
-// ===== MIDDLEWARE =====
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// ===== TEST ROOT =====
-app.get("/", (req: Request, res: Response) => {
-    res.send("Core API is running with TypeScript!");
-});
+// Test Root
+app.get("/", (req: Request, res: Response) => res.send("Core API running"));
 
-// ===== ROUTES =====
+// Routes
 app.use("/auth", authRoutes);
 
-// ===== SWAGGER DOCS =====
+// Swagger
 const PORT = Number(process.env.PORT) || 5000;
 swaggerDocs(app, PORT);
 
-// ===== START SERVER =====
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
