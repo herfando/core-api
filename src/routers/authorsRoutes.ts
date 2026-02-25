912d88a206e0e67804a637539e95ddc27a7f061d
Herfando <herfando_91@yahoo.com>
2026-02-25 14:54:15 +0700

    new file:   src/controllers/authorsController.ts         new file:   src/controllers/categoriesController.ts         modified:   src/routers/authorsRoutes.ts

--- src/routers/authorsRoutes.ts
+++ src/routers/authorsRoutes.ts
@@ -1,15 +1,6 @@
-// src/routers/authorsRoutes.ts
-import { Router } from 'express';
-import sql from '../models/db';
+// authorsRoutes.ts
+import { Router } from "express";
+import { listAuthors } from "../controllers/authorsController";
 const router = Router();
-
-router.get('/', async (req, res) => {
-    try {
-        const authors = await sql`SELECT * FROM authors`;
-        res.json({ success: true, data: authors });
-    } catch (err) {
-        res.status(500).json({ success: false, message: 'Server error' });
-    }
-});
-
+router.get("/", listAuthors);
 export default router;
